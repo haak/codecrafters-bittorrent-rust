2b179d6085badd53b549087d29d6941064be54e8
Alex Kelleher <halkelleher@gmail.com>
2023-09-08 14:06:26 +0200

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,24 @@ use std::env;
 // #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
+    // println!("encoded_value: {}", encoded_value);
+    // i52e
     if encoded_value.chars().next().unwrap().is_digit(10) {
         // Example: "5:hello" -> "5"
         let colon_index = encoded_value.find(':').unwrap();
+        println!("colon_index: {}", colon_index);
+
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
+    } else if encoded_value.chars().next().unwrap() == 'i' {
+        // Example: "i52e" -> "52"
+        // extract number and check if there is an e after
+        let end_index = encoded_value.find('e').unwrap();
+        let number_string = &encoded_value[1..end_index];
+        let number = number_string.parse::<i64>().unwrap();
+        return serde_json::Value::Number(serde_json::Number::from(number));
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -36,5 +47,18 @@ fn main() {
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::decode_bencoded_value;
+
+    #[test]
+    fn test_decode_bencode_value() {
+        assert_eq!(decode_bencoded_value("5:hello"), "hello");
+        assert_eq!(decode_bencoded_value("1:s"), "s");
+        assert_eq!(decode_bencoded_value("i52e"), 52);
+        assert_eq!(decode_bencoded_value("i3e"), 3);
     }
 }
